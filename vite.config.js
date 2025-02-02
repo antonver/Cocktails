c3bfdcbc1f32902f1a5b0e1c59243db1e5d18382
antonver <anton.verbovyi@gmail.com>
2025-02-02 23:52:18 +0100

    Generating...

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,34 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+  ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src")
-    }
+      "@": path.resolve(__dirname, "./src"),
+      // Add more aliases if needed:
+      // components: path.resolve(__dirname, "./src/components"),
+    },
+  },
+  build: {
+    target: "es2020",
+    minify: "terser",
+    sourcemap: true,
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 4173,
+    open: true
   }
 });
